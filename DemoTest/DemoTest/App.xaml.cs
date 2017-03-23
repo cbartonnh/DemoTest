157b62912e9e8026f0e4cb76c67e6265a1f99ab0
Unknown <cbartonnh@gmail.com>
2017-03-23 14:55:13 -0400

    Intial Checkin

---
+++ DemoTest/DemoTest/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Unity;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using DemoTest.Views;
+
+namespace DemoTest
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer)
+        {
+
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            MobileCenter.Start("ios=20d16c23-31ff-4862-8293-04c7f758e83b;",
+                   typeof(Analytics), typeof(Crashes));
+        }
+    }
+}
---
+++ DemoTest/DemoTest/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace DemoTest.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
